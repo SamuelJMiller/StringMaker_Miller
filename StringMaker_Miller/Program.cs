5f1aef6a873be8745d2ef6b01f196aa5e660a129
samuel miller <samuel.miller@student.centralia.edu>
2021-11-23 19:02:16 -0800

    Add project files.

---
+++ StringMaker_Miller/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// Samuel Miller
+// IT113
+// NOTES: none
+// BEHAVIORS NOT IMPLIMENTED AND WHY: none
+
+namespace StringMaker_Miller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
