98fae642280f2ed420fc1c064004da8c22a6cd89
samuel miller <samuel.miller@student.centralia.edu>
2021-11-25 10:10:48 -0800

    Finished

--- StringMaker_Miller/Program.cs
+++ StringMaker_Miller/Program.cs
@@ -19,8 +19,12 @@ namespace StringMaker_Miller
             StringManager sm = new StringManager();
 
             wr("'Sunbeam Tiger' reversed without case location preservation: " + sm.reverse("Sunbeam Tiger"));
-
+            wr("String representation of current input: " + sm.ToString());
             wr("'Sunbeam Tiger' reversed with case location preservation: " + sm.reverse("Sunbeam Tiger", true));
+            wr("Does 'Sunbeam Tiger' equal the current input? " + sm.Equals("Sunbeam Tiger"));
+            wr("Is 'ABBA' symmetric? " + sm.symmetric("ABBA"));
+            wr("Is 'ABA' symmetric? " + sm.symmetric("ABA"));
+            wr("Is 'ABba' symmetric? " + sm.symmetric("ABba"));
         }
     }
 }
--- StringMaker_Miller/StringManager.cs
+++ StringMaker_Miller/StringManager.cs
@@ -10,11 +10,16 @@ namespace StringMaker_Miller
     {
         string current_input = string.Empty;
 
+        char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        string[] num_words = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+
         Stack<char> stack = new Stack<char>();
         Queue<int> queue = new Queue<int>();
 
         public string reverse(string s)
         {
+            current_input = s;
+
             string final = string.Empty;
 
             char[] array = s.ToCharArray();
@@ -33,6 +38,8 @@ namespace StringMaker_Miller
 
         public string reverse(string s, bool preserve_case)
         {
+            current_input = s;
+
             if (preserve_case == true)
             {
                 string final = string.Empty;
@@ -76,17 +83,55 @@ namespace StringMaker_Miller
 
         public bool symmetric(string s)
         {
-            return true;
+            current_input = s;
+
+            return (s == reverse(s)); // *Applause sounds*
         }
 
         public override string ToString()
         {
-            return string.Empty;
+            int ascii_sum = 0;
+
+            if (current_input == string.Empty)
+            {
+                return "Negative One";
+            } else
+            {
+                char[] array = current_input.ToCharArray();
+
+                for ( int i = 0; i < array.Length; ++i )
+                {
+                    ascii_sum += array[i];
+                }
+
+                return vocalize_int(ascii_sum);
+            }
         }
 
         public override bool Equals(object obj)
         {
-            return true;
+            if (obj is string)
+            {
+                return ((string)obj == current_input);
+            } else
+            {
+                return false;
+            }
+        }
+
+        private string vocalize_int(int num)
+        {
+            string final = string.Empty;
+            string num_string = num.ToString();
+
+            char[] array = num_string.ToCharArray();
+
+            for ( int i = 0; i < array.Length; ++i )
+            {
+                final += num_words[Array.IndexOf(digits, array[i])] + ' ';
+            }
+
+            return final;
         }
     }
 }
